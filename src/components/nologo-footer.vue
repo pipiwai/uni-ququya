bf35db1bc00c95dc9d80bd41377e55d775c5ae2c
pipiwai <137573000@qq.com>
2026-03-12 03:27:07 +0800

    网站名称修改

--- src/components/nologo-footer.vue
+++ src/components/nologo-footer.vue
@@ -17,7 +17,7 @@
             <view class="flex items-center justify-center space-x-2">
                 <image src="https://linglan008-blog.oss-cn-hangzhou.aliyuncs.com/%E5%8F%AF%E8%BE%BE%E9%B8%AD.png"
                     class="w-5 h-5 rounded-full" alt="Logo" />
-                <text class="text-sm text-gray-500 ml-2">趣去水印工具 © 2026</text>
+                <text class="text-sm text-gray-500 ml-2">趣趣鸭去水印工具 © 2026</text>
             </view>
         </view>
     </view>
